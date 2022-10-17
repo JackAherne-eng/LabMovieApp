80c3119abf510a1540fc79bf6ba8b50c993702de
Jack Aherne <jack.aherne03@gmail.com>
2022-10-17 16:37:04 +0100

    Added filtering of movie list

--- moviesApp/src/components/filterMoviesCard/index.js
+++ moviesApp/src/components/filterMoviesCard/index.js
@@ -39,7 +39,7 @@ const formControl =
   
     const handleChange = (e, type, value) => {
       e.preventDefault()
-      // Completed later
+      props.onUserInput(type, value)   // NEW
     };
     const handleTextChange = e => {
       handleChange(e, "name", e.target.value)
